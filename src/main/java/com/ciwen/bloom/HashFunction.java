573774beb9ba2eeb29f9713d9281fbc479f0dc09
ciwen <1956952765@qq.com>
2024-12-30 13:09:05 +0800

    feat: 新增HashFunction的接口内容

--- src/main/java/com/ciwen/bloom/HashFunction.java
+++ src/main/java/com/ciwen/bloom/HashFunction.java
@@ -1,5 +1,12 @@
 package com.ciwen.bloom;
 
 /*哈希函数的接口定义*/
-public interface HashFunction {
+public interface HashFunction<T> {
+    /**
+     * 计算哈希值
+     * @param item 要哈希的数据
+     * @param seed 种子值，用于生成不同的哈希值
+     * @return 哈希值
+     */
+    int hash(Object item, int seed);
 }
